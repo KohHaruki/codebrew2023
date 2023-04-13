d0e4adf588c7f09f41b11e402812fd6aaccaad2c
Haruki Koh <harukikoh@gmail.com>
2023-04-13 16:32:37 +1000

    Implemented test /prompt endpoint

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
+// Sets up the express app
 const express = require("express");
 const app = express();
 
-const { json } = require("express");
+// Reads the .env file, which stores the OPEN_AI_API_KEY
+// Reference: https://www.npmjs.com/package/dotenv 
+require("dotenv").config();
 
-// ========== EXAMPLE ============
+// OPEN AI
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const MODEL = "gpt-3.5-turbo";
+
+const openai = new OpenAIApi(configuration);
+
+// This is an example. Don't uncomment this section, otherwise it will send API call
+// const completion = await openai.createChatCompletion({
+//   model: "gpt-3.5-turbo",
+//   messages: [{role: "user", content: "Hello world"}],
+// });
+// console.log(completion.data.choices[0].message);
+
+// ========== EXAMPLE ============)
 app.get("/", (req, res) => {
   res.send("Server is Running");
 });
@@ -14,6 +35,42 @@ app.get("/user", (req, res) => {
 });
 // ========== EXAMPLE END ===========
 
+// POST request to Open AI chatgpt-3.5-turbo
+app.post("/prompt", async (req, res) => {
+  const userPrompt = req.body.prompt; // Get's user's prompt/question
+  const completePrompt = `
+    User's prompt: ${userPrompt}.
+    Answer the user's prompt exclusively in JSON format with following properties:
+    {
+      ELI5: string,
+      Description: string,
+      Example: string,
+      Prerequisites: []
+    }
+  `;
+
+  const promptResponse = await openai.createChatCompletion({
+    model: MODEL,
+    messages: [
+      {
+        role: "system",
+        content: "role: a knowledgeable assistant in any topic imaginable. Audience: high school and university students."
+      }, 
+      {
+        role: "user",
+        content: completePrompt
+      }],
+  });
+
+  // TODO: convert response into JSON and send it back to front-end
+
+  console.log(promptResponse.data.choices[0].message);
+  res.status(200).send(promptResponse.data.choices[0].message);
+});
+
 app.listen(8080, () => {
   console.log("Listening on port 8080");
+  console.log("OPEN_AI_API_KEY: ", process.env.OPEN_AI_API_KEY) // Make sure you have .env file with OPEN_AI_API_KEY
 });
+
+
