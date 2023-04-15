571a964522633b5e574e5874ed864785ec0225b3
Haruki Koh <harukikoh@gmail.com>
2023-04-15 14:43:48 +1000

    minor change

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <div className="fixed bottom-0 w-full z-10 border-t-2 text-xs text-gray-500 text-center py-2 px-3 flex justify-between bg-neutral-50">
-            <p>Made with React, TypeScript, Tailwind CSS, ChatGPT</p>
-            <p>By Cyber Sapiens</p>
+            <p>Made with React, Express.js, TypeScript, Tailwind CSS, ChatGPT</p>
+            <p>Learn Smart GPT By Cyber Sapiens</p>
         </div>
     )
 }
